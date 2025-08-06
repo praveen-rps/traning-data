5bc0a8fbbaac8b41e34fcf5b69c2455d3391708d
Your Name <you@example.com>
2025-08-06 17:35:26 +0530

    added parameters to url

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
+
+@app.route("/welcome/<name>/<city>")
+def greeting(name,city):
+    return render_template('welcome.html', sname=name, scity=city)
 
 @app.route("/")
 def home():
