029f0e71b5a0d14ec2ea3a6265791c7d3aff0c6a
Your Name <you@example.com>
2025-08-06 10:43:03 +0530

    added restdemoapi

---
+++ angular-projects/restdemo/src/app/user.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private url = "https://jsonplaceholder.typicode.com/users/"
+
+  constructor(private http:HttpClient) { }
+
+  getUser(id:number) : Observable<User>{
+    return this.http.get<User>(this.url+id)
+  }
+}
